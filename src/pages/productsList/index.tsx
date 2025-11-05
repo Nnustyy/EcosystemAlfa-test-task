7d1a8ec046d1c37c95547f212a0490bf60640d6d
Anastasia <anastasiaerofeeva121@gmail.com>
2025-11-05 21:15:28 +0300

    products list

--- src/pages/productsList/index.tsx
+++ src/pages/productsList/index.tsx
@@ -1,17 +1,34 @@
-import React, { useEffect } from 'react';
 import { useGetProductsQuery } from '../../app/services/api';
+import ProductCard from '../productCard';
 
 const ProductsList = () => {
 
-const {data} = useGetProductsQuery();
+const {data, isLoading, error} = useGetProductsQuery();
 
-  useEffect(() => {
-    console.log(data)
-  },[data])
+
+  if(error) {
+    console.log(error)
+  }
 
   return (
-    <div>
-      ProductsList
+    
+    <div> {isLoading 
+      ?
+      <div className="flex justify-center items-center min-h-64">
+      <svg className="animate-spin h-8 w-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    </div>
+      :
+      <div className='gap-2 grid grid-cols-2 sm:grid-cols-4'>
+        {data &&
+        data.map((product) => 
+          <ProductCard price={product.price} title={product.title} key={product.id} image={product.images} />
+        )
+        }
+      </div>
+      }
     </div>
   );
 };
