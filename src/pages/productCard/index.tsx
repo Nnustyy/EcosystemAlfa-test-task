267d6ca4d69eb676393457ec853fc904b71b2fb9
Anastasia <anastasiaerofeeva121@gmail.com>
2025-11-05 21:15:03 +0300

    product card

--- src/pages/productCard/index.tsx
+++ src/pages/productCard/index.tsx
@@ -1,10 +1,30 @@
+import { Card, CardBody, CardHeader, Image } from '@heroui/react';
 import React from 'react';
 
-const ProductCard = () => {
+type Props = {
+  price?:number,
+  title?:string,
+  image?:string | string[]
+}
+
+
+const ProductCard = ({price, title, image}:Props) => {
   return (
-    <div>
-      Product card
-    </div>
+    <Card className='py-4'>
+      <CardHeader className='overflow-visible py-2'>
+      <Image
+      alt='product photo'
+      className='object-cover rounded-xl'
+      width={270}
+      src={image}
+      
+      />
+      </CardHeader>
+      <CardBody className='pb-0 pt-2 px-4 flex-col items-start'>
+    <h4 className="font-bold text-large">{title}</h4>
+    <p className="text-tiny uppercase font-bold">${price}</p>
+      </CardBody>
+    </Card>
   );
 };
 
